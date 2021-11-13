d725c8cfacaa2205972835d9abd21d7435616c91
Chang Ze-Rong <kchang1@imsa.edu>
2021-11-13 14:36:57 -0600

    added js functions

--- email-script.js
+++ email-script.js
@@ -1,5 +1,11 @@
 
 // chooses and displays the correct draft
 function openDraft(number) {
-    console.log("open draft " + number);
+    console.log("open draft " + number); // print statement
+    document.getElementById("myForm").style.display = "block";
 }
+
+
+  function closeForm() {
+    document.getElementById("myForm").style.display = "none";
+  }
