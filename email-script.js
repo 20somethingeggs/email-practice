117705e297258ac384c3815f3cba1aabb16de5e2
iakella <ishaakella@gmail.com>
2021-11-13 15:43:18 -0600

    OpenDraft Update

--- email-script.js
+++ email-script.js
@@ -2,6 +2,17 @@
 // chooses and displays the correct draft
 function openDraft(number) {
     console.log("open draft " + number); // print statement
+
+    const draft_array = ["Draft1", "Draft2", "Draft3"];
+    if(number == 1) {
+      console.log(draft_array[0]);
+    }
+    if(number == 2) {
+      console.log(draft_array[1]);
+    }
+    if(number == 3) {
+      console.log(draft_array[2]);
+    }
     document.getElementById("myForm").style.display = "block";
 }
 
