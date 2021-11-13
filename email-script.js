c9e6b7fee853be2b25da2f494ec7630f5aa76aab
Chang Ze-Rong <kchang1@imsa.edu>
2021-11-13 15:49:36 -0600

    added first draft template

--- email-script.js
+++ email-script.js
@@ -1,8 +1,13 @@
 
+// find the right array entry 
+// 1 - get which button clicked 
+// 2 - decide based on that which draft to get
+//   - if number == 1 then get index 0 of array
 // chooses and displays the correct draft
 function openDraft(number) {
     console.log("open draft " + number); // print statement
     document.getElementById("myForm").style.display = "block";
+    
 }
 
 
