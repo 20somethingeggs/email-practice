198a82a938aa82f424495a7e7db08a357a7434e4
Chang Ze-Rong <kchang1@imsa.edu>
2022-01-28 15:50:22 -0600

    starting files for forgot password/login

---
+++ login-script.js
@@ -0,0 +1,9 @@
+function forgot() {
+    document.getElementById("login").style.display = "none"; 
+    document.getElementById("forgot-password").style.display = "block"; 
+}
+
+function login() {
+    document.getElementById("login").style.display = "block"; 
+    document.getElementById("forgot-password").style.display = "none"; 
+}
